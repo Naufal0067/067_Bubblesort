b5ad33b4b49fc1dc95f89304fd14b2363dfe042a
Naufal0067 <naufalopang1928@gmail.com>
2024-03-08 15:48:17 +0700

    procedur untuk mengurutkan array dengan metode bubblesort

--- 067_Bubblesort/067_Bubblesort.cpp
+++ 067_Bubblesort/067_Bubblesort.cpp
@@ -27,8 +27,35 @@ void input() {     //procedur untuk input
 
 	for (int i = 0; i, n; i++) { // Looping dengan i dimulai dari 0 hingga n-1
 		cout << "Data ke-" << (i + 1) << ": "; //Output ke layar 
-		cin >> arr[i];
+		cin >> arr[i];          //output dari pengguna
 
 	}
+
 }
+
+void BubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubblesort
+	int pass = 1; //step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) { //step 3
+				int temp; 
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+		pass = pass + 1; // step 4
+
+		cout << "\nPass" << pass - 1 << ": "; // number of pass
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";  // Mneampilkan data pada number of pass
+		}
+		cout << endl;
+	} while (pass <= n - 1);  //step 5
+
+}
+
+
 
