689e85f7e228e84a30a7e10f8d7a25b113cb909a
Naufal0067 <naufalopang1928@gmail.com>
2024-03-08 15:56:53 +0700

    menambahkan procedur display

--- 067_Bubblesort/067_Bubblesort.cpp
+++ 067_Bubblesort/067_Bubblesort.cpp
@@ -57,5 +57,24 @@ void BubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubbl
 
 }
 
+void display() {
+	cout << endl;
+	cout << "==================================" << endl;
+	cout << " Element Array yang telah tersusun" << endl;
+	cout << "==================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j]; // menampilkan array
+		if (j < n - 1) {
+			cout << " -->";
+		}
+	}
+	cout << endl;
+	cout << endl;
 
+	cout << " jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+	cout << endl;
+	cout << endl;
+
+}
 
